8d9a31758f7adeb410f2ad175562d3dd9197d441
Mariia Bilinska <mariiaklochko@gmail.com>
2022-02-10 15:16:08 +0000

    Step 2

--- page_loader/page_loader.py
+++ page_loader/page_loader.py
@@ -2,25 +2,51 @@
 
 import os
 import requests
+from bs4 import BeautifulSoup
+from pathlib import Path
 
 
-def get_file_name(url):
-    """Get a name of a file containing downloaded webpage.
+def get_name(url):
+    """Derive a name of a directory or file from an url.
 
     Parameters:
         url: webpage url.
 
     Returns:
-        name of a file containing the downloaded webpage.
+        directory or file name as a string.
     """
     address = url.split('//')[1]
-    filename = [
+    name = [
         '-' if not i.isalpha() and not i.isdigit() else i for i in address
     ]
-    return ''.join(filename) + '.html'
+    return ''.join(name)
 
 
-def get_webpage_content(url):
+def get_directory_name(name):
+    """Get directory name containing downloaded webpage data.
+
+    Parameters:
+        name: name derived from url.
+
+    Returns:
+        directory name as a string.
+    """
+    return name + '_files'
+
+
+def get_file_name(name):
+    """Get a name of a file containing downloaded webpage.
+
+    Parameters:
+        name: name derived from url.
+
+    Returns:
+        name of a file containing the downloaded webpage.
+    """
+    return name + '.html'
+
+
+def get_webpage_contents(url):
     """Request data from a webpage.
 
     Parameters:
@@ -44,20 +70,74 @@ def write_to_file(filepath, webpage_content):
         file_.write(webpage_content)
 
 
+def download_images(image_links, directory_path, url):
+    """Download images from a website.
+
+    Parameters:
+        image_links: list of image sources,
+        directory_path: path to the dir where the image will be stored.
+
+    Returns:
+        list of relative pathes to images.
+    """
+    list_of_img_relative_pathes = []
+    for link in image_links:  # link = '/assets/professions/nodejs.png'
+        r = requests.get(url + link).content
+#        image_name = link[::-1].split('/')[0][::-1]  # nodejs.png
+
+        src = link[:-len(Path(link).suffix)]
+        # src = '/assets/professions/nodejs'
+        link_name = ''.join([
+            '-' if not i.isalpha() and not i.isdigit() else i for i in src
+        ])  # -assets-professions-nodejs
+        image_name = get_name(url) + link_name + Path(link).suffix
+
+        path_to_image = os.path.join(directory_path, image_name)
+        with open(path_to_image, 'wb+') as img:
+            img.write(r)
+
+        relative_path_to_image = (
+            f'{get_directory_name(get_name(url))}/{image_name}'
+        )
+        list_of_img_relative_pathes.append(relative_path_to_image)
+
+    return list_of_img_relative_pathes
+
+
 def download(url, directory_path='current'):
     """Get path to file with a saved webpage.
 
     Parameters:
         dir_path: path to a directory with downloaded webpage,
         url: url of a webpage to be downloaded.
-
-    Returns:
-        path to file with a saved webpage
     """
-    webpage_content = get_webpage_content(url)
-    filename = get_file_name(url)
+    webpage_content = get_webpage_contents(url)
+    filename = get_file_name(get_name(url))
+    directoryname = get_directory_name(get_name(url))
     if directory_path == 'current':
-        directory_path = os.getcwd()
+        current = os.getcwd()
+        directory_path = os.path.join(current, directoryname)
+    else:
+        directory_path = os.path.join(directory_path, directoryname)
+    os.mkdir(directory_path)  # path/to/webpage-url_files
+    # filepath for future HTML file
     filepath = os.path.join(directory_path, filename)
-    write_to_file(filepath, webpage_content)
+
+    file_contents = BeautifulSoup(webpage_content, 'html.parser')
+    images = file_contents.find_all('img')  # find all the images
+    list_of_images = []
+    for link in images:
+        list_of_images.append(link.get('src'))  # get image sources
+    # download all the images from the file AND return list of pathes to images:
+    list_of_image_relative_pathes = download_images(
+        list_of_images,
+        directory_path,
+        url,
+    )
+    hash_table = dict(zip(list_of_images, list_of_image_relative_pathes))
+    for link in images:
+        link['src'] = hash_table[link['src']]
+
+    write_to_file(filepath, file_contents.prettify())  # creating HTML file
+
     return filepath
--- tests/test_page_loader.py
+++ tests/test_page_loader.py
@@ -1,7 +1,8 @@
-from page_loader.page_loader import download
-from page_loader.page_loader import get_webpage_content
+from page_loader.page_loader import download, get_file_name
+from page_loader.page_loader import get_directory_name, get_name
+from page_loader.page_loader import get_webpage_contents
 from page_loader.page_loader import write_to_file
-import requests_mock
+# import requests_mock
 import tempfile
 import os
 
@@ -10,7 +11,7 @@ def read_file(filepath):
     """Read a file.
 
     Parameters:
-        filepath; path to file.
+        filepath: path to file.
 
     Returns:
         data from file.
@@ -19,40 +20,61 @@ def read_file(filepath):
         return file_.read()
 
 
-def tests_page_loader_write_to_file():
+def tests_get_directory_name():
+    "Test get_directory_name function in page_loader module"
+    name = get_name('https://page-loader.hexlet.repl.co')
+    correct_answer = 'page-loader-hexlet-repl-co_files'
+    received_filepath = get_directory_name(name)
+    assert received_filepath == correct_answer
+
+
+def tests_get_file_name():
+    "Test get_file_name function in page_loader module"
+    name = get_name('https://page-loader.hexlet.repl.co')
+    correct_answer = 'page-loader-hexlet-repl-co.html'
+    received_filepath = get_file_name(name)
+    assert received_filepath == correct_answer
+
+
+def tests_write_to_file():
     "Test write_to_file function in page_loader module"
     with tempfile.TemporaryDirectory() as temporary_directory:
-        correct_answer = read_file('tests/fixtures/downloaded_website.html')
-        webpage_content = get_webpage_content('https://page-loader.hexlet.repl.co')
-        filepath = os.path.join(temporary_directory, 'page-loader-hexlet-repl-co.html')
+        directory_path = os.path.join(
+            temporary_directory,
+            get_directory_name(get_name('https://page-loader.hexlet.repl.co')),
+        )
+        os.mkdir(directory_path)
+        correct_answer = read_file('tests/fixtures/original_website.html')
+        webpage_content = get_webpage_contents('https://page-loader.hexlet.repl.co')
+        filepath = os.path.join(
+            temporary_directory,
+            'page-loader-hexlet-repl-co_files/page-loader-hexlet-repl-co.html',
+        )
         write_to_file(filepath, webpage_content)
         received = read_file(filepath)
         assert received == correct_answer
 
 
-
-def tests_page_loader_get_file_name():
-    "Test get_file_name function in page_loader module"
-    with tempfile.TemporaryDirectory() as temporary_directory:
-        correct_answer = os.path.join(temporary_directory, 'page-loader-hexlet-repl-co.html')
-        received_filepath = download(
-            'https://page-loader.hexlet.repl.co',
-            temporary_directory,
-        )
-        assert received_filepath == correct_answer
-
-
-def tests_page_loader_download():
+def tests_download():
     "Test download function in page_loader module"
     with tempfile.TemporaryDirectory() as temporary_directory:
-        with requests_mock.Mocker() as mock_:
-            correct_answer = read_file('tests/fixtures/downloaded_website.html')
-            # instead of requests
-            mock_.get('https://page-loader.hexlet.repl.co', text=correct_answer)
-            download('https://page-loader.hexlet.repl.co', temporary_directory)
-            # => page-loader-hexlet-repl-co.html inside temporary dir
-            received = read_file(os.path.join(
-                temporary_directory,
-                'page-loader-hexlet-repl-co.html',
-            ))
-            assert received == correct_answer
+        correct_answer = read_file('tests/fixtures/downloaded_website.html')
+        download('https://page-loader.hexlet.repl.co', temporary_directory)
+        directory_path = os.path.join(
+            temporary_directory,
+            get_directory_name(get_name('https://page-loader.hexlet.repl.co')),
+        )
+        # check if 'page-loader-hexlet-repl-co_files' already exists
+        if not os.path.isdir(directory_path):
+            os.mkdir(directory_path)
+        received = read_file(os.path.join(
+            directory_path,
+            'page-loader-hexlet-repl-co.html',
+        ))
+        assert received == correct_answer
+        assert os.path.isfile(
+            os.path.join(
+                directory_path,
+                'page-loader-hexlet-repl-co-assets-professions-nodejs.png',
+            )
+        )
