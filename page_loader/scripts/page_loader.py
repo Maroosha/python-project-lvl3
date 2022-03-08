b39256cb65135b855e90e8d4d5a4bcb6cd5e3545
Mariia Bilinska <mariiaklochko@gmail.com>
2022-03-08 16:13:10 +0000

    fixing errors

--- page_loader/scripts/page_loader.py
+++ page_loader/scripts/page_loader.py
@@ -9,6 +9,7 @@ from page_loader.page_loader import download
 from page_loader.functions import KnownError
 
 LOG_FILE = 'page_loader.log'
+FINAL_MESSAGE = '\nPage was successfully downloaded into {}'
 
 
 def main():
@@ -27,7 +28,7 @@ def main():
     parser.add_argument('url')
     args = parser.parse_args()
     try:
-        download(args.url, args.output)
+        filepath = download(args.url, args.output)
     except KnownError as known_error:
         logging.error(known_error)
         print('ATTENTION: An error occured: %s.', known_error)
@@ -37,6 +38,11 @@ def main():
         print('ATTENTION: An unexpected error occured. \
 For details, see %s.', LOG_FILE)
         sys.exit(1)
+    else:
+        final_message = FINAL_MESSAGE.format(filepath)
+        print(final_message)
+        logging.info(final_message)
+        sys.exit(0)
 
 
 if __name__ == '__main__':
